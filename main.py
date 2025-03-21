b1dafb3bd6e1ff5858c5ee72eedf70c6111cde67
owls4all <owls4all@gmail.com>
2025-03-21 08:51:57 -0700

    did excercise

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ Every person's unique talents are valued equally.
 Everyone takes responsibility for themselves and their community."""
 
 # Make a dictionary where the keys are characters and the values are the number of times that character appears in the `text` print the dicitonary
+
+dictionary = {}
+for letter in "abcdefghijklmnopqrstuvwxyz":
+    dictionary[letter] = text.lower().count(letter)
+print(dictionary)
